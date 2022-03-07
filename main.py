d0b74a50b8dd7cf1249cece477ce0f9610d87303
thegamebegins25 <wdppostcard@gmail.com>
2022-03-07 17:30:03 -0500

    added stuff

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
-#opens the file with the comm.
-file = open(r"new.txt", "r")
-lines = file.read().splitlines()
+def getall(fname):
+    #opens the file with all the vocab
+    file = open(fname+".txt", "r")
+    allL = file.read().splitlines()
+    #loop
+    all = {}
+    for i in allL:
+        #turns the vocab into a dictionary
+        split = i.split("	")
+        print(split)
+        all[split[0]] = split[1]
+    return all
 
-#filters out extra lines
-lines = list(filter(("").__ne__, lines))
-print(lines)
 
-#opens the file with all the vocab
-file = open(r"all.txt", "r")
-allL = file.read().splitlines()
-#loop
-all = {}
-for i in allL:
-    #turns the vocab into a dictionary
-    split = i.split("	")
-    print(split)
-    all[split[0]] = split[1]
-print(all)
-#gets the def of the terms in the comm. list
-final = ""
-for i in lines:
-    add = i + "\g" + all[i] + "\n"
-    final = final + add
+def com():
+    #opens the file with the comm.
+    file = open(r"new.txt", "r")
+    lines = file.read().splitlines()
 
-#adds to output file
-file = open(r"out.txt", "w")
-file.writelines(final)
-    
+    #filters out extra lines
+    lines = list(filter(("").__ne__, lines))
+    print(lines)
+    all = getall("all")
+    final = ""
+    for i in lines:
+        add = i + " " + all[i] + "\n"
+        final = final + add
+
+    #adds to output file
+    file = open(r"out.txt", "w")
+    file.writelines(final)
+    print("Go to out.txt")
+
+com()
